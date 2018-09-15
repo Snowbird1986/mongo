980063e6f10c4e2fd1ee01b50fddd9bc498cf970
Snowbird <jakounter@yahoo.com>
2018-09-15 13:22:48 -0500

    a

--- server.js
+++ server.js
@@ -74,43 +74,6 @@ app.get("/scrape", function(req, res) {
       });
     });
   });
-  
-  // axios.get("http://www.espn.com/nfl/team/_/name/kc/").then(function(response) {
-  //   // Then, we load that into cheerio and save it to $ for a shorthand selector
-  //   var $ = cheerio.load(response.data);
-
-  //   // Now, we grab every h2 within an article tag, and do the following:
-  //   $("article div div h1").each(function(i, element) {
-  //     // Save an empty result object
-  //     var result = {};
-
-  //     // Add the text and href of every link, and save them as properties of the result object
-  //     result.title = $(this)
-  //       .children("a")
-  //       .text();
-  //     result.link = $(this)
-  //       .children("a")
-  //       .attr("href");
-  //     result.summary = $(this)
-  //       .parent()
-  //       .children("p")
-  //       .text();
-
-  //     // Create a new Article using the `result` object built from scraping
-  //     db.Article.create(result)
-  //       .then(function(dbArticle) {
-  //         // View the added result in the console
-  //         console.log(dbArticle);
-  //       })
-  //       .catch(function(err) {
-  //         // If an error occurred, send it to the client
-  //         return res.json(err);
-  //       });
-  //   });
-
-  //   // If we were able to successfully scrape and save an Article, send a message to the client
-  //   res.send("Scrape Complete");
-  // });
 });
 
 // Route for getting all Articles from the db
