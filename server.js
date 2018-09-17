bb6af2d24d75cbb3678cb4f7045ae4f47366834e
Snowbird <jakounter@yahoo.com>
2018-09-16 19:55:10 -0500

    a

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongohomework");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongohomework";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
